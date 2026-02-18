a17b9d18c8abb7096d1fabe935830e22db2507d0
Alessandro Panait <14529922@itisrossi.vi.it>
2026-02-18 16:49:56 +0100

    Update 06.asm

--- 06.asm
+++ 06.asm
@@ -11,39 +11,40 @@ secondo db "il secondo è maggiore $"
 caporiga db 13,10,'$'
 
 .code
+main proc
+    ; inizializza segmento dati
+    mov ax, @data
+    mov ds, ax
 
 STAMPA macro str
-  lea dx, str
-  mov ah, 09h 
-  int 21h
+    lea dx, str
+    mov ah, 09h
+    int 21h
 endm
 
 LEGGI macro num
-  mov ah, 01h
-  int 21h
-  sub al, '0'
-  mov num, al
+    mov ah, 01h
+    int 21h
+    sub al, '0'
+    mov num, al
 endm
 
-.startup
+    STAMPA msg1
+    LEGGI num1
+    STAMPA caporiga
 
-STAMPA msg1
-LEGGI num1
-STAMPA caporiga
+    STAMPA msg2
+    LEGGI num2
+    STAMPA caporiga
 
-STAMPA msg2
-LEGGI num2
-STAMPA caporiga
-
-cmp num1, num2
-jg PRIM
-STAMPA secondo
-jmp FINE
+    cmp num1, num2
+    jg PRIM
+    STAMPA secondo
+    jmp FINE
 PRIM:
-STAMPA primo
+    STAMPA primo
 FINE:
     mov ah, 4Ch
     int 21h
-
-.exit
-end
+main endp
+end main
