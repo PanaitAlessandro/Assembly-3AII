b271064fa8638a82624889538ceb29bdc1d2a1f2
Alessandro Panait <14529922@itisrossi.vi.it>
2026-02-18 17:20:04 +0100

    Update 07.asm

--- 07.asm
+++ 07.asm
@@ -2,27 +2,30 @@
 .stack 100h
 
 .data
-msg1 db "inserisci base: $"
-msg2 db "inserisci altezza: $"
-msg3 db "inserisci lato obliquo: $"
+msg1 db "Inserisci base: $"
+msg2 db "Inserisci altezza: $"
+msg3 db "Inserisci lato obliquo: $"
 
-h db ?
-b db ?
-lo db ?
+b db ?       ; base
+h db ?       ; altezza
+lo db ?      ; lato obliquo
 area db ?
 perimetro db ?
 
 msgarea db "Area: $"
 msgperi db "Perimetro: $"
-
 caporiga db 13,10,'$'
 
 .code
+
+; --- macro per stampare stringhe ---
 STAMPA macro msg
   lea dx, msg
   mov ah, 09h
   int 21h
 endm
+
+; --- macro per leggere un numero singolo (0-9) ---
 LEGGIN macro num
   mov ah, 01h
   int 21h
@@ -30,44 +33,51 @@ LEGGIN macro num
   mov num, al
 endm
 
+; --- macro per stampare un numero singolo (0-9) ---
 STAMPAN macro val
   mov ah, 02h
   add al, '0'
   int 21h
 endm
 
-  .startup
+.startup
 
-  STAMPA msg1
-  LEGGIN b
-  STAMPA caporiga
+; --- input base ---
+STAMPA msg1
+LEGGIN b
+STAMPA caporiga
 
-  STAMPA msg2
-  LEGGIN h
-  STAMPA caporiga
+; --- input altezza ---
+STAMPA msg2
+LEGGIN h
+STAMPA caporiga
 
-  STAMPA msg3
-  LEGGIN lo
-  STAMPA caporiga
+; --- input lato obliquo ---
+STAMPA msg3
+LEGGIN lo
+STAMPA caporiga
 
-  ; area
-  mov al, b
-  mul h
-  mov area, al
+; --- calcolo area (base * altezza) ---
+mov al, b
+mul h          ; AL * h -> AX
+mov area, al
 
-  STAMPA msgarea
-  STAMPAN area
-  STAMPA caporiga
+STAMPA msgarea
+mov al, area
+STAMPAN al
+STAMPA caporiga
 
-  ; perimetro
+; --- calcolo perimetro (2*base + 2*lato obliquo) ---
+mov al, b
+add al, b      ; AL = 2*base
+add al, lo
+add al, lo     ; AL = 2*base + 2*lo
+mov perimetro, al
 
-  mov bl, b
-  add bl, lo
-  add bl, b
-  add bl, lo
-  mov perimetro, bl
+STAMPA msgperi
+mov al, perimetro
+STAMPAN al
+STAMPA caporiga
 
-  STAMPA msgperi
-  STAMPAN perimetro
-  .exit
+.exit
 end
